df98cd93b1dc82eef90ccab099a2618c58af485f
Igor Korshuk <igorkorshuk@coherentsolutions.com>
2018-03-13 21:48:53 +0300

    corpses

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const fileUpload = require('express-fileupload');
 const openDB = require('json-file-db');
 const jsonfile = require('jsonfile');
 
-var db = {
-    places: openDB('db/places.json'),
-    profiles: openDB('db/profiles.json'),
-    pupils: openDB('db/pupils.json')
-};
+const dbfile = 'db/json.json';
+let db = {};
+
+jsonfile.readFile(dbfile, function(err, obj) {
+    db = {
+        places: obj.places,
+        profiles: obj.profiles,
+        pupils: obj.pupils,
+        corpses: createCorpses(obj.places)
+    };
+});
+
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -17,20 +24,52 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .post('/upload', onFileUpload)
-    .get('/api/pupils/list', function (req, res) {
-        db.pupils.get(function(err, data){
-            sendResp(res, data);
-        });
+    .get('/api/search', function (req, res) {
+        const search = req.query.search;
+        let data = [];
+        
+        if (search.length > 0) {
+            data = db.pupils
+                        .filter(function(pupil){
+                            return pupil.firstName.indexOf(search) > -1;
+                        })
+                        .map(function(pupil){
+                            pupil.profile = 'df';
+                            return pupil;     
+                        });
+        }
+        sendResp(res, []);
     })
-    .get('/api/profiles/list', function (req, res) {
-        db.profiles.get(function(err, data){
-            sendResp(res, data);
-        });
+    .get('/api/pupils', function (req, res) {
+        const query = req.query;
+        const corpsQuery = req.query.corps;
+        let corps = {}
+        let placesQuery = [];
+
+        if (corpsQuery.length) {
+            corps = db.corpses.find(function(element) {
+                return element.alias === corpsQuery;
+            })
+            for(let i = 0; i < corps.places.length; i++) {
+                console.log('@@',corps.places[i]._id )
+                placesQuery.push(corps.places[i]._id)
+            }
+
+           // db.pupils.filter(function)
+        }
+
+        console.log(corps, placesQuery)
+        
+        sendResp(res, db.pupils);
     })
-    .get('/api/places/list', function (req, res) {
-        db.places.get(function(err, data){
-            sendResp(res, data);
-        });
+    .get('/api/profiles', function (req, res) {
+        sendResp(res, db.profiles);
+    })
+    .get('/api/corpses', function (req, res) {
+        sendResp(res, db.corpses);
+    })
+    .get('/api/places', function (req, res) {
+        sendResp(res, db.places);
     })
     .get('/', function (req, res) {
         res.render('pages/index')
@@ -51,25 +90,25 @@ function onFileUpload(req, res) {
     }
 
 
-    var dbFile = req.files.dbFile;
+    var uploadedFile = req.files.dbFile;
 
     // Use the mv() method to place the file somewhere on your server
-    dbFile.mv('db/json.json', function(err) {
+    uploadedFile.mv(dbfile, function(err) {
         if (err) {
             return res.status(500).send(err);
         } else {
-
-            var dbfile = 'db/json.json';
-
             jsonfile.readFile(dbfile, function(err, obj) {
-                jsonfile.writeFileSync('db/pupils.json', obj.pupils);
-                jsonfile.writeFileSync('db/profiles.json', obj.profiles);
-                jsonfile.writeFileSync('db/places.json', obj.places);
+
+               // jsonfile.writeFileSync('db/pupils.json', obj.pupils);
+              //  jsonfile.writeFileSync('db/profiles.json', obj.profiles);
+              //  jsonfile.writeFileSync('db/places.json', obj.places);
+              //  jsonfile.writeFileSync('db/corpses.json', corpses);
 
                 db = {
-                    places: openDB('db/places.json'),
-                    profiles: openDB('db/profiles.json'),
-                    pupils: openDB('db/pupils.json')
+                    places: obj.places,
+                    profiles: obj.profiles,
+                    pupils: obj.pupils,
+                    corpses: createCorpses(obj.places)
                 };
             });
 
@@ -77,4 +116,63 @@ function onFileUpload(req, res) {
             res.redirect('/');
         }
     });
+}
+
+function createCorpses (places) {
+    let corpsesMap = {};
+    let corpses = [];
+    let corps;
+    let i = 0;
+    let length = places.length;
+
+    for (i; i < length; i++) {
+        corps = places[i];
+        if (corpsesMap[corps.name]) {
+            corpsesMap[corps.name].places.push(corps)
+        } else {
+            corpsesMap[corps.name] = {
+                name: corps.name,
+                alias: toUTF8Array(corps.name),
+                places: [corps]
+            }
+        }
+    }
+    for (corps in corpsesMap) {
+        if (corpsesMap.hasOwnProperty(corps)) {
+            corpses.push(corpsesMap[corps]);
+        }
+    }
+    console.log(corpses)
+    return Array.from(corpses);
+}
+
+function toUTF8Array(str) {
+    var utf8 = [];
+    for (var i=0; i < str.length; i++) {
+        var charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6), 
+                      0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(0xe0 | (charcode >> 12), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                      | (str.charCodeAt(i) & 0x3ff))
+            utf8.push(0xf0 | (charcode >>18), 
+                      0x80 | ((charcode>>12) & 0x3f), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+    }
+    return utf8.join('');
 }
